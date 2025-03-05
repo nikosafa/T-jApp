2f53ad3f3970edb3b53ba4a60198eac88407cf5d
nikosafa <nikosafa7895@gmail.com>
2025-03-05 12:56:22 +0100

    Navbar

--- App/app/src/main/java/com/example/tjapp/MainActivity.kt
+++ App/app/src/main/java/com/example/tjapp/MainActivity.kt
@@ -4,14 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.Alignment
 import com.example.tjapp.ui.theme.TøjAppTheme
+
+// Import af ikoner
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
+
+// Farver fra Figma
+val PageBackground = Color(0xFFF9F4EE)
+val NavBarBackground = Color.White
+
+// En klasse for items i nav bar.
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,31 +41,85 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TøjAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainScreen() {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home
+        ),
+        BottomNavigationItem(
+            title = "Search",
+            selectedIcon = Icons.Filled.Search,
+            unselectedIcon = Icons.Outlined.Search
+        ),
+        BottomNavigationItem(
+            title = "Upload",
+            selectedIcon = Icons.Filled.AddCircle,
+            unselectedIcon = Icons.Outlined.AddCircle
+        ),
+        BottomNavigationItem(
+            title = "Inbox",
+            selectedIcon = Icons.Filled.Email,
+            unselectedIcon = Icons.Outlined.Email
+        ),
+        BottomNavigationItem(
+            title = "Profile",
+            selectedIcon = Icons.Filled.Person,
+            unselectedIcon = Icons.Outlined.Person
+        )
     )
+
+    var selectedItemIndex by remember { mutableStateOf(0) }
+
+    Scaffold( // Et element ligesom Column, skaber et grundlæggende layout for appen, eksempelvis en bottom bar.
+        containerColor = PageBackground,
+        bottomBar = {
+            NavigationBar(
+                containerColor = NavBarBackground,
+            ) {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItemIndex == index,
+                        onClick = { selectedItemIndex = index },
+                        icon = {
+                            Icon(
+                                imageVector = if (selectedItemIndex == index) {
+                                    item.selectedIcon
+                                } else {
+                                    item.unselectedIcon
+                                },
+                                contentDescription = item.title
+                            )
+                        },
+                        label = { Text(text = item.title) }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(PageBackground),
+            contentAlignment = Alignment.Center
+        ) {
+        }
+    }
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewBottomNavigation() {
     TøjAppTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
